e9e48b8ec898b19240b66854bc89df751ac0f6de
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2021-02-14 12:35:11 +0100

    fixed style

--- commands.py
+++ commands.py
@@ -1,7 +1,9 @@
-import utils
-from replit import db
-from functools import wraps
 from datetime import datetime
+from functools import wraps
+
+from replit import db
+
+import utils
 
 all_commands = ['all', 'All', 'soon', 'Soon']
 get_commands = ['g', 'G', 'get', 'Get']
@@ -147,7 +149,6 @@ def reset_timer(successor=None):
     while True:
         msg = yield msg_to_send
         if msg.length == 1:
-            msg_to_send = ''
             boss = msg.content[0]
             if boss in utils.BOSSES:
                 default_timer = utils.BOSSES[boss]
@@ -158,21 +159,21 @@ def reset_timer(successor=None):
         elif successor is not None:
             msg_to_send = successor.send(msg)
 
+
 @start_chain
 def when_boss(successor=None):
     msg_to_send = None
     while True:
         msg = yield msg_to_send
         if msg.length == 2 and msg.content[0] in when_commands:
-            msg_to_send = ''
             boss = msg.content[1]
             if boss in utils.BOSSES:
                 timer = utils.get_timer(boss)
                 if timer is not None:
-                  msg_to_send = f'{boss} due at {datetime.fromtimestamp(timer * 60)} gt'
+                    msg_to_send = f'{boss} due at {datetime.fromtimestamp(timer * 60)} gt'
                 else:
-                  msg_to_send = f'{boss} no timer set'
+                    msg_to_send = f'{boss} no timer set'
             else:
-                 msg_to_send = f'{boss} is not tracked'
+                msg_to_send = f'{boss} is not tracked'
         elif successor is not None:
             msg_to_send = successor.send(msg)
