69775beaf1a78fa251355422d08651fb18229dec
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2021-02-15 17:39:32 +0000

    made api a single call for multiple bosses

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from replit import db
 
 from utils import BOSSES
@@ -13,26 +13,28 @@ def home():
     return '<h1>Alive</h1>'
 
 
-@app.route('/api')
+@app.route('/api', methods=['POST'])
 def api():
-    response = Response()
     api_key = request.headers.get('X-ApiKey', type=str)
     api_keys = db['api_keys']
-
+    res_bosses = {}
     for user, api_key_db in api_keys.items():
         if api_key == api_key_db:
-            boss = request.args['boss']
-            print(f'API: {user} requested {boss} at {datetime.now()}')
-            if boss in BOSSES:
-                response.status_code = 200
-                response.data = str(db[boss])
-            else:
-                response.status_code = 404
+            json = request.json
+            req_bosses = json['bosses']
+            print(f'API: {user} {json} at {datetime.now()}')
+            for boss in req_bosses:
+              if boss in BOSSES:
+                  res_bosses[boss] = db[boss]
+              else:
+                  res_bosses[boss] = 404
             break
     else:
+        response = Response()
         response.status_code = 401
-
-    return response
+        return response
+        
+    return jsonify(res_bosses)
 
 
 def run():
