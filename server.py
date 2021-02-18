9e02fdaf4d296a078f16c841306515aeabb0c025
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2021-02-18 09:59:08 +0000

    refactored api_get started api_set

--- server.py
+++ server.py
@@ -3,39 +3,63 @@ from datetime import datetime
 from flask import Flask, request, Response, jsonify
 from replit import db
 
-from utils import BOSSES
+import utils
+
+
+def auth(api_key):
+	api_keys = db['api_keys']
+	for user, key in api_keys.items():
+		if api_key == key:
+			return user
+			break
+	else:
+		return None
+		
+
 
 app = Flask('')
 
 
 @app.route('/')
 def home():
-    return '<h1>Alive</h1>'
+	return '<h1>Alive</h1>'
 
 
-@app.route('/api', methods=['POST'])
-def api():
-    api_key = request.headers.get('X-ApiKey', type=str)
-    api_keys = db['api_keys']
-    res_bosses = {}
-    for user, api_key_db in api_keys.items():
-        if api_key == api_key_db:
-            json = request.json
-            req_bosses = json['bosses']
-            print(f'API: {user} {json} at {datetime.now()}')
-            for boss in req_bosses:
-                if boss in BOSSES:
-                    res_bosses[boss] = db[boss]
-                else:
-                    res_bosses[boss] = 404
-            break
-    else:
-        response = Response()
-        response.status_code = 401
-        return response
+@app.route('/api/get', methods=['POST'])
+def api_get():
+	api_key = request.headers.get('X-ApiKey', type=str)
+	res_bosses = {}
+	user = auth(api_key)
+	if user is not None:
+		json = request.json
+		req_bosses = json['bosses']
+		print(f'API: {user} {json} at {datetime.now()}')
+		for boss in req_bosses:
+			res_bosses[boss] = utils.get_timer(boss)
+	else:
+		response = Response()
+		response.status_code = 401
+		return response
 
-    return jsonify(res_bosses)
+	return jsonify(res_bosses)
+
+
+@app.route('/api/set', methods=['POST'])
+def api_set():
+	api_key = request.headers.get('X-ApiKey', type=str)
+	user = auth(api_key)
+	if user is not None:
+		boss = request.form['boss']
+		print(f'API: {user} {boss} at {datetime.now()}')
+		for boss in req_bosses:
+			res_bosses[boss] = utils.get_timer(boss)
+	else:
+		response = Response()
+		response.status_code = 401
+		return response
+
+	return jsonify(res_bosses)
 
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+	app.run(host='0.0.0.0', port=8080)
