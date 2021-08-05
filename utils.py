5675a8b945d8db57157aca9b53ea0449c9a67794
ArgoPanoptes <>
2021-08-05 15:41:37 +0000

    removed comments

--- utils.py
+++ utils.py
@@ -140,8 +140,6 @@ class Message:
 def logger(msg):
     log = f'[{datetime.now()}] {msg}'
     print(log)
-    # with open('log.txt', 'a') as logs:
-    #logs.write(log + '\n')
     db['logs'] = db['logs'] + log + '\n'
 
 
