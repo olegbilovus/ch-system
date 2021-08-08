781b177b1a34411f2468be3e3d77a1498e86ce29
ArgoPanoptes <>
2021-08-08 10:32:26 +0000

    bug fixes

--- main.py
+++ main.py
@@ -17,39 +17,45 @@ chain = commands.reset_timer(commands.set_timer_2(commands.default()))
 
 @client.event
 async def on_ready():
-    routine.delete_logs()
-    utils.status(False)
-    utils.logger('BOT: logged')
+	routine.delete_logs()
+	utils.status(False)
+	utils.logger('BOT: logged')
 
 
 @client.event
 async def on_message(message):
-    if db['429']:
-        utils.status(True)
-        time.sleep(utils._429)
-        utils.status(False)
-        return
+	if db['429']:
+		utils.status(True)
+		time.sleep(utils._429)
+		utils.status(False)
+		return
 
-    if message.author == client.user or message.channel.name != 'timer_bot' or str(
-            message.author) in BOTS_NAMES:
-        return
+	if message.author == client.user or message.channel.name != 'timer_bot' or str(
+	    message.author) in BOTS_NAMES:
+		return
 
-    utils.logger(f'{message.author.display_name}: {message.content}')
-    msg = utils.Message(message.content.lower().split(' '), message.author)
-    global chain
-    msg_to_send = chain.send(msg)
-    utils.logger(msg_to_send)
+	utils.logger(f'{message.author.display_name}: {message.content}')
+	msg = utils.Message(message.content.lower().split(' '), message.author)
+
+	global chain
+	try:
+		msg_to_send = chain.send(msg)
+	except Exception as e:
+		chain = commands.reset_timer(commands.set_timer_2(commands.default()))
+		logger(str(e))
+
+	utils.logger(msg_to_send)
 
 
 server_s = Thread(target=server.run)
 server_s.start()
 
 try:
-    client.run(os.getenv('TOKEN'))
+	client.run(os.getenv('TOKEN'))
 except discord.errors.HTTPException as e:
-    message_error = str(e)
-    utils.logger(message_error)
-    if '429' in message_error:
-        utils.status(True)
-        time.sleep(utils._429)
-        utils.status(False)
+	message_error = str(e)
+	utils.logger(message_error)
+	if '429' in message_error:
+		utils.status(True)
+		time.sleep(utils._429)
+		utils.status(False)
