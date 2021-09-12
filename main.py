1cd52a7be52bc83b48d7f6834f7be3d32d4e1bda
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2021-09-12 19:41:53 +0000

    style fixes

--- main.py
+++ main.py
@@ -152,8 +152,8 @@ def change_role():
         if user and user['role'] <= 4:
             if db_utils.change_role(user_id, role):
                 return response
-            else:
-                response.status_code = 404
+            response.status_code = 404
+            return response
         else:
             response.status_code = 404
             return response
