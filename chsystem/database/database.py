9cf648a22a2d38b1fe43b09f6cd09c1f88e0bdf8
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-07-10 20:15:16 +0200

    added the remaining classes to complete the API to communicate with the db

--- chsystem/database/database.py
+++ chsystem/database/database.py
@@ -44,7 +44,7 @@ class Database:
 
 class Server(Database):
 
-    def retrieve_all(self):
+    def get_all(self):
         self.cur.execute('SELECT * FROM server')
         return self.cur.fetchall()
 
@@ -127,5 +127,108 @@ class ApiKey(Database):
 
     def delete(self, user_id):
         self.cur.execute('DELETE FROM apikey WHERE userprofileid = %s', (user_id,))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+
+class DiscordID(Database):
+
+    def get_by_user_id(self, user_id):
+        self.cur.execute('SELECT * FROM discordid WHERE userprofileid = %s', (user_id,))
+        return self.cur.fetchone()
+
+    def create(self, user_id, discord_id):
+        self.cur.execute('INSERT INTO discordid (userprofileid, discordid) VALUES (%s, %s)', (user_id, discord_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def update(self, user_id, discord_id):
+        self.cur.execute('UPDATE discordid SET discordid = %s WHERE userprofileid = %s', (discord_id, user_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete(self, user_id):
+        self.cur.execute('DELETE FROM discordid WHERE userprofileid = %s', (user_id,))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+
+class DiscordChannelID(Database):
+    def get_by_clan_id(self, clan_id):
+        self.cur.execute('SELECT * FROM discordchannelid WHERE clanid = %s', (clan_id,))
+        return self.cur.fetchone()
+
+    def create(self, clan_id, discord_channel_id):
+        self.cur.execute('INSERT INTO discordchannelid (clanid, discordchannelid) VALUES (%s, %s)',
+                         (clan_id, discord_channel_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def update(self, clan_id, discord_channel_id):
+        self.cur.execute('UPDATE discordchannelid SET discordchannelid = %s WHERE clanid = %s',
+                         (discord_channel_id, clan_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete(self, clan_id):
+        self.cur.execute('DELETE FROM discordchannelid WHERE clanid = %s', (clan_id,))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+
+class Timer(Database):
+
+    def get_by_clan_id(self, clan_id):
+        self.cur.execute('SELECT * FROM timer WHERE clanid = %s', (clan_id,))
+        return self.cur.fetchone()
+
+    def get_by_clan_id_and_boss_name(self, clan_id, boss_name):
+        self.cur.execute('SELECT * FROM timer WHERE clanid = %s AND bossName = %s', (clan_id, boss_name))
+        return self.cur.fetchone()
+
+    def create(self, boss_name, boss_type, timer, server_id, clan_id):
+        self.cur.execute('INSERT INTO timer (bossName, type, timer, serverid, clanid) VALUES (%s, %s, %s, %s, %s)',
+                         (boss_name, boss_type, timer, server_id, clan_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def update(self, clan_id, boss_name, timer):
+        self.cur.execute('UPDATE timer SET timer = %s WHERE clanid = %s AND bossName = %s',
+                         (timer, clan_id, boss_name))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete(self, clan_id, boss_name):
+        self.cur.execute('DELETE FROM timer WHERE clanid = %s AND bossName = %s', (clan_id, boss_name))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete_by_clan_id(self, clan_id):
+        self.cur.execute('DELETE FROM timer WHERE clanid = %s', (clan_id,))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+
+class Subscriber(Database):
+    def get_by_user_id(self, user_id):
+        self.cur.execute('SELECT * FROM subscriber WHERE userprofileid = %s', (user_id,))
+        return self.cur.fetchone()
+
+    def get_by_timer_id(self, timer_id):
+        self.cur.execute('SELECT * FROM subscriber WHERE timerid = %s', (timer_id,))
+        return self.cur.fetchone()
+
+    def create(self, user_id, timer_id):
+        self.cur.execute('INSERT INTO subscriber (userprofileid, timerid) VALUES (%s, %s)', (user_id, timer_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete(self, user_id, timer_id):
+        self.cur.execute('DELETE FROM subscriber WHERE userprofileid = %s AND timerid = %s', (user_id, timer_id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete_by_user_id(self, user_id):
+        self.cur.execute('DELETE FROM subscriber WHERE userprofileid = %s', (user_id,))
         self.conn.commit()
         return self.cur.fetchone()
