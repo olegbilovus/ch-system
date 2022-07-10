4a17f8cc72b1cd45a7488a96defc7a27608d3a67
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-07-10 19:00:00 +0200

    made easier to add future tables that will share same parameters to connect to the db

--- chsystem/database/database.py
+++ chsystem/database/database.py
@@ -4,33 +4,38 @@ import requests
 
 import psycopg2
 
-from datetime import datetime
-
 logging.basicConfig(format='%(levelname)s %(asctime)s - %(message)s', level=logging.INFO)
 
 
 class Database:
     db_url = os.getenv('DB_URL')
     db_uri = os.getenv('DB_URI')
-    conn = psycopg2.connect(db_uri)
-    cur = conn.cursor()
+    conn = None
+    cur = None
 
-    def __init__(self):
-        self.update_url()
+    def __init__(self, uri=None, url=None):
+        self.update_url(uri, url)
 
-    def update_url(self, force=False):
-        res = requests.get(self.db_url, data={'update': '1' if force else '0'})
-        if res.status_code == 200:
-            os.putenv('DB_URI', res.text)
-            self.db_uri = res.text
-            logging.info('Got DB_URL')
-            self.conn = psycopg2.connect(self.db_uri)
-            self.cur = self.conn.cursor()
+    def update_url(self, uri=None, url=None, force=False):
+        if uri is None and Database.db_uri is None:
+            res = requests.get(Database.db_url if url is None else url, data={'update': '1' if force else '0'})
+            if res.status_code == 200:
+                os.putenv('DB_URI', res.text)
+                Database.db_uri = res.text
+                logging.info('Got DB_URL')
+            else:
+                logging.error('ERROR DB_URL')
+                Database.db_uri = None
+                self.conn = None
+                self.cur = None
         else:
-            logging.error('ERROR DB_URL')
-            self.db_uri = None
-            self.conn = None
-            self.cur = None
+            Database.db_uri = uri if uri is not None else Database.db_uri
+            Database.db_url = url if url is not None else Database.db_url
+
+        self.conn = psycopg2.connect(Database.db_uri)
+        self.cur = self.conn.cursor()
+        self.cur.execute('SELECT version()')
+        logging.info(self.cur.fetchone())
 
 
 class Server(Database):
