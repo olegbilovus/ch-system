cb24b01e82ab2d3e631daf10ab30ede5b75f733a
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-07-13 21:56:25 +0200

    added more logs and refactored some names

--- chsystem/notify/notify.py
+++ chsystem/notify/notify.py
@@ -7,16 +7,16 @@ import database
 
 timer_db = database.Timer()
 subscriber_db = database.Subscriber()
-notifyWebhook_db = database.NotifyWebhook()
+clan_discord_db = database.ClanDiscord()
 
 logger = logs.get_logger('Notify', token=os.getenv('LOGTAIL_NOTIFY'))
 
 USERNAME = 'Notifier'
 
 logger.info('Check')
-webhooks = notifyWebhook_db.get_all()
+webhooks = clan_discord_db.get_all_notify_webhooks()
 
-for clan_id, webhook in webhooks:
+for clan_id, webhook, discord_guild_id in webhooks:
     timers = timer_db.get_notify_data_by_clan_id(clan_id)
     for timer_id, timer, boss_name in timers:
         subscribers = subscriber_db.get_discord_ids_by_timer_id_clan_id(timer_id)
@@ -25,7 +25,7 @@ for clan_id, webhook in webhooks:
             msg += f'<@{discord_id}>'
 
         res = requests.post(webhook, data={'username': USERNAME, 'content': msg})
-        logger.info(f'response: {res.status_code}, sent: {msg}')
+        logger.info(f'GuildID: {discord_guild_id}, ClanID: {clan_id}, response: {res.status_code}, sent: {msg}')
 
 timer_db.close()
 logger.info('Finish check')
